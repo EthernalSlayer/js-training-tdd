c5fbd684998ea08b6103ab54128bab2722fda896
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-06-11 23:14:25 +0200

    resolve cut exercise

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,50 @@
  */
 
 // Your code:
+const cutFirst = (str) => {
+    let newStr = '';
+    for (let i = 2; i < str.length; i++) {
+        newStr += str[i];
+    }
+    return newStr;
+};
+
+const cutLast = (str) => {
+    let newStr = '';
+    for (let i = 0; i < str.length - 2; i++) {
+        newStr += str[i];
+    }
+    return newStr;
+};
+
+const cutFirstLast = (str) => {
+    let newStr = '';
+    for (let i = 2; i < str.length - 2; i++) {
+        newStr += str[i];
+    }
+    return newStr;
+};
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof cutFirst, 'function');
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst('toto'), 'to');
+assert.strictEqual(cutFirst('hahaha'), 'haha');
+assert.strictEqual(cutFirst('123'), '3');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutLast, 'function');
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutLast('toto'), 'to');
+assert.strictEqual(cutLast('saturn'), 'satu');
+assert.strictEqual(cutLast('123'), '1');
+
+assert.strictEqual(typeof cutFirstLast, 'function');
+assert.strictEqual(cutFirstLast.length, 1);
+assert.strictEqual(cutFirstLast('toto'), '');
+assert.strictEqual(cutFirstLast('saturn'), 'tu');
+assert.strictEqual(cutFirstLast('123'), '');
+assert.strictEqual(cutFirstLast('history'), 'sto');
+
+//assert.fail('You must write your own tests');
 // End of tests */
