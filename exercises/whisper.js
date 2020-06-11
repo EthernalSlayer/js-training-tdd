916b0894de9766e5bdac30e46d0a226d5f2d7885
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-06-11 21:03:32 +0200

    resolve whisper exercise

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -8,9 +8,17 @@
  */
 
 // Your code:
-
+const whisper = (str) => {
+    return str.toLowerCase();
+};
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof whisper, 'function');
+assert.strictEqual(whisper.length, 1);
+assert.strictEqual(whisper('KID'), 'kid');
+assert.strictEqual(whisper('Toto'), 'toto');
+assert.strictEqual(whisper('xoxo'), 'xoxo');
+assert.strictEqual(whisper('123'), '123');
+//assert.fail('You must write your own tests');
 // End of tests */
